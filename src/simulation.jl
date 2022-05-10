a64105a9525f84ed5857522d4648d7b5705e0fb7
bjoern <git@wotanii.de>
2022-05-10 08:44:57 +0200

    move agent-update to separete function. cleanup

--- src/simulation.jl
+++ src/simulation.jl
@@ -11,48 +11,47 @@ module MySimulation
     lk_sim = ReentrantLock()
     lk_ctrl = ReentrantLock()
 
+    function get_step_agents(simStep::SimulationStep, ctrlState::ControlState)
+        agent_list_result = []
+
+        for agent in simStep.agent_list
+            if ( 0 == mod(floor(simStep.num_step / 10), 2))
+                agent_pos_x = agent.pos_x + 0.01
+                agent_pos_y = agent.pos_y + 0.01
+            else
+                agent_pos_x = agent.pos_x - 0.01
+                agent_pos_y = agent.pos_y - 0.01
+            end
+            push!(agent_list_result, Agent(agent_pos_x, agent_pos_y, agent.direction_angle, agent.size))
+        end
+
+        return agent_list_result
+    end
+
     function simulationLoop!(simState_transfer::Ref{SimulationState}, ctrlState::ControlState)
         @info "simulationLoop!..."
 
-        #simState_old = deepcopy(simState_transfer[])
         simState = simState_transfer[].last_step[]
-        last_time = Base.time_ns()
+        last_time_ns = Base.time_ns()
         while !ctrlState.is_stop
             
-            num_age = simState.num_step + 1
-            simState_agent1_pos_x = 0
-            simState_agent1_pos_y = 0
-            if ( 0 == mod(floor(num_age / 10), 2))
-                simState_agent1_pos_x = simState.agent_list[1].pos_x + 0.01
-                simState_agent1_pos_y = simState.agent_list[1].pos_y + 0.01
-            else
-                simState_agent1_pos_x = simState.agent_list[1].pos_x - 0.01
-                simState_agent1_pos_y = simState.agent_list[1].pos_y - 0.01
-            end
+            agentList = get_step_agents(simState, ctrlState)
 
-            
-            new_time = Base.time_ns()
-            elapsed_time = new_time-last_time
-            last_frame_time_ms = elapsed_time/1000
+            last_frame_time_ms = (Base.time_ns()-last_time_ns) / 1000
             time_to_wait_s = (ctrlState.min_frametime_ms - last_frame_time_ms) / 1000
-            if time_to_wait_s>0
+            if time_to_wait_s > 0
                 sleep(time_to_wait_s)
             end
 
-            agentList = [ 
-                Agent(simState_agent1_pos_x, simState_agent1_pos_y, simState.agent_list[1].direction_angle, simState.agent_list[1].size ),
-                Agent(simState.agent_list[2].pos_x, simState.agent_list[2].pos_y, simState.agent_list[2].direction_angle, simState.agent_list[2].size)
-            ]
             lock(lk_sim)
             try
                 simState_transfer[].last_step=Ref(simState)
             finally
                 unlock(lk_sim)
             end
-            simState = SimulationStep(num_age, agentList, last_frame_time_ms)
+            simState = SimulationStep(simState.num_step + 1, agentList, last_frame_time_ms)
 
-            last_time = Base.time_ns()
-
+            last_time_ns = Base.time_ns()
         end
     
         @info "simulationLoop!... done"
