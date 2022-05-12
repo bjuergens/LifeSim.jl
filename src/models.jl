0d2375b6e8f863586a6983e0672417580a9b97c8
bjuergens <bjoern.juergens@outlook.com>
2022-05-12 15:26:07 +0200

    move tests to sourcefile

--- src/models.jl
+++ src/models.jl
@@ -1,58 +1,75 @@
 
 module MyModels
-    export Agent, makeAgent
-    export SimulationState, ControlState, SimulationStep, Vec2
+export Agent, makeAgent
+export SimulationState, ControlState, SimulationStep, Vec2
 
-    using CImGui: IM_COL32
+using CImGui: IM_COL32
 
-    "position in simulation-space"
-    Vec2 = @NamedTuple{x::Cfloat,y::Cfloat}
+"position in simulation-space"
+Vec2 = @NamedTuple{x::Cfloat,y::Cfloat}
 
-    mutable struct Agent
-        pos::Vec2
-        direction_angle:: Cfloat
-        speed::Cfloat
-        size::Cfloat
-        color::UInt32
-        id::Int
-    end
-
-    struct SimulationStep
-        num_step::Int
-        agent_list::Vector{Agent}
-        last_frame_time_ms::Float64
-    end
-
-    mutable struct SimulationState
-        last_step::Ref{SimulationStep}
-    end
-
-
-    mutable struct ControlState
-        arr::Vector{Cfloat}
-        is_stop::Bool
-        afloat::Cfloat
-        min_frametime_ms::Cfloat
-        request_simulation_state_at_age::Int
-    end
+mutable struct Agent
+    pos::Vec2
+    direction_angle:: Cfloat
+    speed::Cfloat
+    size::Cfloat
+    color::UInt32
+    id::Int
 end
 
-"""viable example for all models"""
+struct SimulationStep
+    num_step::Int
+    agent_list::Vector{Agent}
+    last_frame_time_ms::Float64
+end
+
+mutable struct SimulationState
+    last_step::Ref{SimulationStep}
+end
+
+mutable struct ControlState
+    arr::Vector{Cfloat}
+    is_stop::Bool
+    afloat::Cfloat
+    min_frametime_ms::Cfloat
+    request_simulation_state_at_age::Int
+end
+end #module MyModels
+
+
+"""viable example for all models used by testclasses for most other modules in this repo"""
 module MyModelExamples
-    export ctrlState, simState, aAgent, bAgent, stepStep
-    using ..MyModels
-    using CImGui: IM_COL32
-    aAgent = Agent((x=0.3, y=0.3), pi/2, 0.01, 0.11, IM_COL32(11,11,0,255),1)
-    bAgent = Agent((x=0.6, y=0.6), 2*pi, 0.02, 0.13, IM_COL32(22,22,0,255),2)   
-    ctrlState = ControlState(Cfloat[sin(x) for x in 0:0.05:2pi], false,0.9, 50.0, 5)
-    stepStep = SimulationStep(1, [aAgent, bAgent], 0.1)
-    simState = SimulationState(stepStep)
-end
+export ctrlState, simState, aAgent, bAgent, stepStep
+using ..MyModels
+using CImGui: IM_COL32
+aAgent = Agent((x=0.3, y=0.3), pi/2, 0.01, 0.11, IM_COL32(11,11,0,255),1)
+bAgent = Agent((x=0.6, y=0.6), 2*pi, 0.02, 0.13, IM_COL32(22,22,0,255),2)   
+ctrlState = ControlState(Cfloat[sin(x) for x in 0:0.05:2pi], false,0.9, 50.0, 5)
+stepStep = SimulationStep(1, [aAgent, bAgent], 0.1)
+simState = SimulationState(stepStep)
+end #module MyModelExamples
 
 
+module ModelTests
+# Testing-module is needed as workaround for error "ERROR: LoadError: UndefVarError: @safetestset not defined"
+# when macro is called in toplevel-block that is not a module.
+using SafeTestsets
+export doTest
+function doTest()
+@safetestset "Examples" begin
+    using ...MyModels
+    using ...MyModelExamples
     
+    @test 0<aAgent.pos.x<1
+    @test 1+1==2  # canary
+end
+end
+end #module ModelTests
+
 if abspath(PROGRAM_FILE) == @__FILE__
-    using ..MyModelExamples
+    using .MyModelExamples
     @info "ModelExamples" aAgent bAgent ctrlState simState stepStep
-    include("../tests/models.test.jl")
+
+    using .ModelTests
+    doTest()
 end
