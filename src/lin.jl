85e74b697ad77fed953cc6d01d3264497b71b52f
bjoern <git@wotanii.de>
2022-05-13 14:25:08 +0200

    add debug-msg when testing. more tests

--- src/lin.jl
+++ src/lin.jl
@@ -11,6 +11,7 @@ module LSLin
     using Distances: Euclidean
     
     const dist_euclid = Euclidean() # initiate one instance at compiletime for faster speed, maybe
+    const Ɛ =  1e-15
 
     struct Vec2{T} <: FieldVector{2, T}
         x::T
@@ -67,8 +68,19 @@ module LSLin
         return x/width
     end
 
+    function write_stack()
+        res = []
+        for a in stacktrace()
+            push!(res, a)
+        end
+        return res
+    end
+
     "return angle between vector and x-axis"
     function angle_to_axis(p::Vec2)
+        if p.x < Ɛ && p.y < Ɛ
+            @debug "atan not defined for 0 values" stacktrace()[3:end]
+        end
         return atan(p.x,p.y)
     end
 
@@ -133,15 +145,44 @@ function doTest()
     @test angle_to_axis(xAxis)  ≈ 0
     @test angle_to_axis(yAxis)  ≈ pi/2
 
+    # test Vec2: constructor, similarity and tolerance
     @test Vec2(0.01,0.01) ≈ Vec2(0.,0.) atol=0.02
     @test Vec2(0.01,0.01) ≉ Vec2(0.,0.) atol=0.002
+
+    # free stuff gained from using StaticArrays
+    @test Vec2(1.0,1.0) + Vec2(1.0,1.0) ≈ Vec2(2.0,2.0)
+    @test Vec2(1.0,1.0) * 2.0 ≈ Vec2(2.0,2.0)
+    @test sin.(Vec2(pi,pi))  ≈ Vec2(0,0) atol=1e-15
+    @test sin.(0.5*Vec2(pi,pi))  ≈ Vec2(1,1) atol=1e-15
+
     @test move_in_direction(xAxis, pi/2, 1.) ≈ Vec2(1.,1.) 
     @test move_in_direction(xAxis, pi, 1.) ≈ Vec2(0.,0.) atol=0.00001
 
+    # move 90° from origin
+    @test move_in_direction(Vec2(0,0), 0.5pi, 1.) ≈ Vec2(1.,0.) 
+    @test move_in_direction(Vec2(0,0), 1.0pi, 1.) ≈ Vec2(0.,-1.)
+    @test move_in_direction(Vec2(0,0), 1.5pi, 1.) ≈ Vec2(-1.,0.)
+    @test move_in_direction(Vec2(0,0), 2.0pi, 1.) ≈ Vec2(0.,1.)
+    # different notation
+    @test move_in_direction(Vec2(0,0), pi/2, 1.) ≈ Vec2(1.,0.) 
+    @test move_in_direction(Vec2(0,0), 3pi/2, 1.) ≈ Vec2(-1.,0.)
+    
+    # move 45˚ on unit grid
+    @test move_in_direction(Vec2(3,3), pi/4, sqrt(2)) ≈ Vec2(4.,4.) 
+    @test move_in_direction(Vec2(3,3), -pi/4, sqrt(2)) ≈ Vec2(2.,4.) 
+    @test move_in_direction(Vec2(3,3), 3pi/4, sqrt(2)) ≈ Vec2(4.,2.) 
+    @test move_in_direction(Vec2(3,3), -3pi/4, sqrt(2)) ≈ Vec2(2.,2.) 
+
+    # should wrap around
+    @test move_in_direction(Vec2(3,3), -3pi/4, sqrt(2)) ≈ move_in_direction(Vec2(3,3), -3pi/4 + 2pi, sqrt(2)) 
+
+    # rudimentary test on test-method
     @test distance_manual(Vec2(1,1), Vec2(1,2)) ≈ 1
     @test distance_manual(Vec2(1,1), Vec2(1,0)) ≈ 1
     @test distance_manual(Vec2(0,0), Vec2(1,1)) ≈ sqrt(2)
     @test distance_manual(Vec2(0,0), Vec2(2,2)) ≈ sqrt(8)
+
+    # compare fast euclidean from module with simple test-version
     @test_distance Vec2(0,0) Vec2(1,0)
     @test_distance Vec2(0,0) Vec2(1,1) 
     @test_distance Vec2(0,0) Vec2(2,2) 
@@ -155,6 +196,11 @@ function doTest()
 
     # move one point in the direction of another point by their distance, then the should end up on the same spot
     @test move_in_direction(Vec2(0.0,0.0), angle_to_axis(Vec2(3.0,4.0)), 5.0)  ≈ Vec2(3.0,4.0) atol=0.00001
+
+    @test direction(Vec2(0,0), Vec2(1,0))   ≈ -pi/2
+    @test direction(Vec2(12,3), Vec2(13,3)) ≈ -pi/2
+    # @test direction(Vec2(0,0), Vec2(0,0)) ≈ 0
+    @test direction(Vec2(0,0), Vec2(1,0)) ≈ -pi/2
     
     p1 = Vec2(12,23)
     p2 = Vec2(34,45)
@@ -163,17 +209,22 @@ function doTest()
     @show dir dist
     @test move_in_direction(p1, dir, dist) ≈ p2 broken=true
 
-    # free stuff gained from using StaticArrays
-    @test Vec2(1.0,1.0) + Vec2(1.0,1.0) ≈ Vec2(2.0,2.0)
-    @test Vec2(1.0,1.0) * 2.0 ≈ Vec2(2.0,2.0)
-    @test sin.(Vec2(pi,pi))  ≈ Vec2(0,0) atol=1e-15
-    @test sin.(0.5*Vec2(pi,pi))  ≈ Vec2(1,1) atol=1e-15
+    function myStackOverflowError()
+        # https://github.com/JuliaArrays/StaticArrays.jl/issues/1026
+        f32::Float32 = 1.0
+        f64::Float64 = 1.0
+        pos = Vec2(f32, f64)
+        return true
+    end
+
+    @test myStackOverflowError() broken=true  
 
 end
 end
 end #module LinTests
 
 if abspath(PROGRAM_FILE) == @__FILE__
+    ENV["JULIA_DEBUG"]="lin"
     using .LinTests
     doTest()
 end
