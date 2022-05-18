4d35842e3c7bd3699fa8520b6e789b6137db3b6b
bjoern <git@wotanii.de>
2022-05-18 22:49:09 +0200

    construct neuralnet from genome

--- src/neural.jl
+++ src/neural.jl
@@ -5,7 +5,7 @@ if abspath(PROGRAM_FILE) == @__FILE__
 end
 
 module LSNaturalNet
-export NaturalNet
+export NaturalNet, genome_size
 using StaticArrays
 # using LinearAlgebra
 
@@ -14,6 +14,7 @@ struct NaturalNet{dim_in,dim_N,dim_out,precision<:AbstractFloat}
     V::SMatrix{dim_in, dim_N  , precision}
     W::SMatrix{dim_N , dim_N  , precision}
     T::SMatrix{dim_N , dim_out, precision}
+    genome::SVector
 
     "create dummy object with zero values"
     NaturalNet(;input_dim=2, neural_dim=4, output_dim=3) = new{input_dim,neural_dim,output_dim,Float32}(
@@ -21,9 +22,20 @@ struct NaturalNet{dim_in,dim_N,dim_out,precision<:AbstractFloat}
         , @SMatrix zeros(input_dim,neural_dim)
         , @SMatrix zeros(neural_dim,neural_dim)
         , @SMatrix zeros(neural_dim,output_dim)
+        , @SVector zeros(genome_size(input_dim,neural_dim,output_dim))
+        )
+    NaturalNet(genome::SVector;input_dim=2, neural_dim=2, output_dim=2) = new{input_dim,neural_dim,output_dim,Float32}(
+        Ref(SVector{neural_dim,Float32}(zeros(neural_dim)))
+        , SMatrix( reshape(genome[1:input_dim*neural_dim]                                                                     , Size(input_dim,neural_dim)))
+        , SMatrix( reshape(genome[input_dim*neural_dim+1:input_dim*neural_dim+neural_dim^2]                                   , Size(neural_dim,neural_dim)))
+        , SMatrix( reshape(genome[input_dim*neural_dim+neural_dim^2+1:input_dim*neural_dim+neural_dim^2+neural_dim*output_dim], Size(neural_dim,output_dim)))
+        , @SVector zeros(genome_size(input_dim,neural_dim,output_dim))
         )
 end #struct
 
+function genome_size(dim_in::Int,dim_N::Int,dim_out::Int)
+    return dim_in*dim_N + dim_N*dim_N + dim_N*dim_out
+end
 end #module
 
 
@@ -55,7 +67,18 @@ function doTest()
     @test test_net.neural_state[][1] == 0
     test_net.neural_state[] = SVector{4,Float32}(ones(4))
     @test test_net.neural_state[][1] == 1
-    
+
+    @test genome_size(2,2,2) == 4+4+4
+    @test genome_size(3,2,2) == 6+4+4
+    @test genome_size(2,2,3) == 4+4+6
+    @test genome_size(2,3,2) == 6+9+6
+
+    test_genome_size = genome_size(2,3,4)
+    test_genome = SVector{test_genome_size,Float32}( 1:test_genome_size)
+    test_net2 =  NaturalNet(test_genome, input_dim=2, neural_dim=3, output_dim=4)
+    @test test_net2.V == reshape( 1:6  , (2,3))    
+    @test test_net2.W == reshape( 7:15 , (3,3))    
+    @test test_net2.T == reshape( 16:27, (3,4))
     @test 1+1==2  # canary
 end
 end
