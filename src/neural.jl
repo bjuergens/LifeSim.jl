dad0647f397068b24e71ccc87a8627124100b887
bjoern <git@wotanii.de>
2022-05-18 08:55:56 +0200

    more neuralstate WIP

--- src/neural.jl
+++ src/neural.jl
@@ -7,18 +7,22 @@ end
 module LSNaturalNet
 export NaturalNet
 using StaticArrays
+# using LinearAlgebra
 
-mutable struct NaturalNet
-    input_dim::Int
-    output_dim::Int
-    neural_state::SVector
+struct NaturalNet{dim_in,dim_N,dim_out,precision<:AbstractFloat}
+    neural_state::Ref{SVector{dim_N, precision}}
+    V::SMatrix{dim_in, dim_N  , precision}
+    W::SMatrix{dim_N , dim_N  , precision}
+    T::SMatrix{dim_N , dim_out, precision}
 
-    NaturalNet(;input_dim=3, neural_dim=5, output_dim=4) = new(
-        input_dim,
-        output_dim,
-        @SVector zeros(neural_dim)
+    "create dummy object with zero values"
+    NaturalNet(;input_dim=2, neural_dim=4, output_dim=3) = new{input_dim,neural_dim,output_dim,Float32}(
+        Ref(SVector{neural_dim,Float32}(zeros(neural_dim)))
+        , @SMatrix zeros(input_dim,neural_dim)
+        , @SMatrix zeros(neural_dim,neural_dim)
+        , @SMatrix zeros(neural_dim,output_dim)
         )
-end
+end #struct
 
 end #module
 
@@ -27,12 +31,32 @@ module NaturalNetTests
 export doTest
 using Test
 using ..LSNaturalNet
+# using LinearAlgebra
+using StaticArrays
 function doTest()
 @testset "NaturalNetTest" begin
-    @show NaturalNet()
+
+    vec3d = SA_F32[1, 2, 3 ] 
+    mat2d = SA_F32[1 2; 3 4] 
+    @test mat2d isa SMatrix{2,2,Float32}
+    @test vec3d isa SVector{3,Float32}
+
+    @test length(NaturalNet().V) >  0
+    @test length(NaturalNet().W) >  0
+    @test length(NaturalNet().T) >  0
+
+    # matrices are immuatble
+    @test_throws ErrorException NaturalNet().V[1]=2 
+    @test_throws ErrorException NaturalNet().W[1]=2 
+    @test_throws ErrorException NaturalNet().T[1]=2 
+
+    # neural state can be updated
+    test_net =  NaturalNet()
+    @test test_net.neural_state[][1] == 0
+    test_net.neural_state[] = SVector{4,Float32}(ones(4))
+    @test test_net.neural_state[][1] == 1
+    
     @test 1+1==2  # canary
-    @test NaturalNet().input_dim > 0
-    @test NaturalNet().output_dim > 0
 end
 end
 end #module NaturalNetTests
