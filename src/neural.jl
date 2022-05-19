a161ce17fc5056c706025cdad147c89a4e3386ca
bjoern <git@wotanii.de>
2022-05-19 14:25:14 +0200

    add random initiation of netowrks

--- src/neural.jl
+++ src/neural.jl
@@ -5,7 +5,7 @@ if abspath(PROGRAM_FILE) == @__FILE__
 end
 
 module LSNaturalNet
-export NaturalNet, genome_size, step!
+export NaturalNet, genome_size, step!, init_random_network
 using StaticArrays
 # using LinearAlgebra
 
@@ -36,18 +36,21 @@ struct NaturalNet{dim_in,dim_N,dim_out,precision<:AbstractFloat}
         )
 end #struct
 
-# todo: get genome size from type
 function genome_size(dim_in::Int,dim_N::Int,dim_out::Int)
     return dim_in*dim_N + dim_N*dim_N + dim_N*dim_out
 end
 
-function step!(ctrnn::NaturalNet, input::SVector)
-    net_dim_in,net_dim_N,net_dim_out,net_precision = typeof(ctrnn).parameters
-    ns_data, ns_prec, ns_dim1, ns_dim2 = typeof(ctrnn.neural_state[]).parameters
-    @assert ns_dim2 == net_dim_N
-    @assert ns_prec == net_precision
+function init_random_network(dim_in, dim_N, dim_out)
+    rand_genome_size = genome_size(dim_in,dim_N,dim_out)
+    rand_genome2 = randn((SVector){rand_genome_size, Float32})
+    return NaturalNet(rand_genome2, input_dim=dim_in, neural_dim=dim_N, output_dim=dim_out)
+end
 
-    dydt = (ctrnn.neural_state[]' * ctrnn.W) + ( tanh.(input)' * ctrnn.V )
+"do a single step in a natural net. Update the internal state. 
+@returns output of the network"
+function step!(ctrnn::NaturalNet, input::SVector)
+    input_with_V = tanh.(input)' * ctrnn.V
+    dydt = (ctrnn.neural_state[]' * ctrnn.W) + ( input_with_V )
     ctrnn.neural_state[] = ctrnn.neural_state[]' + ctrnn.delta_t * dydt
     output = max.(0, ctrnn.neural_state[]' * ctrnn.T)
     return output
@@ -62,6 +65,8 @@ using Test
 using ..LSNaturalNet
 # using LinearAlgebra
 using StaticArrays
+
+
 function doTest()
 @testset "NaturalNetTest" begin
 
@@ -98,6 +103,12 @@ function doTest()
     @test test_net2.T == reshape( 16:27, (3,4))
     @test 1+1==2  # canary
 
+    # type within the network should be consistent
+    net_dim_in, net_dim_N, net_dim_out, net_precision = typeof(test_net2).parameters
+    ns_data, ns_prec, ns_dim1, ns_dim2 = typeof(test_net2.neural_state[]).parameters
+    @test ns_dim2 == net_dim_N
+    @test ns_prec == net_precision
+
     # an update step produces non-zero output and updates the neural state
     test_input = SVector{2,Float32}(1:2)
     old_state = deepcopy( test_net2.neural_state[])
@@ -106,6 +117,17 @@ function doTest()
     @test old_state != test_net2.neural_state[]
     @test all(output .!= zeros(4,1))
     @test ndims(output) == ndims(zeros(4,1))
+
+    # generate some random brains
+    for i in 1:10
+        rand_net =  init_random_network(5,11,7)
+        rand_old_state = deepcopy( rand_net.neural_state[])
+        rand_input = randn((SVector){5, Float32})
+        @test rand_old_state == rand_net.neural_state[]
+        rand_output = step!(rand_net, rand_input)
+        @test rand_old_state != rand_net.neural_state[]
+    end
+
 end
 end
 end #module NaturalNetTests
@@ -114,3 +136,4 @@ if abspath(PROGRAM_FILE) == @__FILE__
     using .NaturalNetTests
     doTest()
 end
+
