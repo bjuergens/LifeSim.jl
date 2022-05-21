2dff871e0f6f5098e1653659708a7e28ce32dbe5
bjoern <git@wotanii.de>
2022-05-21 14:46:39 +0200

    playing around with octree

--- src/test123.jl
+++ src/test123.jl
@@ -1,14 +1,18 @@
-
+using CollisionDetection
 using StaticArrays
 
-struct Vec3D{T} <: FieldVector{3, T}
-    x::T
-    y::T
-    z::T
-end
+function x()
+n = 100
+centers =  [2 .* rand(SVector{3,Float64}) .- 1 for i in 1:n] 
+# center = [randn((SVector){3, Float32}) for in in 1:n]
+radii = [0.1*rand() for i in 1:n]
 
-@info "this works" Vec3D(1,2,3)
-@info "this works" Vec3D(Float32(1), Float32(2),Float32(3))
-@info "this works" Vec3D(Float64(1), Float64(2),Float64(3))
-# @info "this fails" Vec3D(Float32(1), Float32(2),Float64(3))
- 
+tree = Octree(centers, radii)
+
+# Given an index, is the corresponding ball eligible?
+pred(i) = all(centers[i].+radii[i] .> 0)
+# Bounding box in the (center,halfside) format supplied for effiency
+bb = @SVector[0.5, 0.5, 0.5], 0.5
+# collect the iterator of admissible indices
+ids = collect(searchtree(pred, tree, bb))
+end
